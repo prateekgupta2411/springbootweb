ad1517e2b6172633c1d28932bcc8df55dbb1718f
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2024-11-18 22:46:01 +0530

    springboot web

---
+++ springbootweb/src/test/java/com/prateek/springootweb/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prateek.springootweb.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
